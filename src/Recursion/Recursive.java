6eeb25d1515c5e7880d360b52d89c3052100367e
Jason Dowling <jasondowling88@gmail.com>
2019-10-06 18:40:14 +0100

    recursive search added

--- src/Recursion/Recursive.java
+++ src/Recursion/Recursive.java
@@ -2,42 +2,57 @@ package Recursion;
 
 public class Recursive {
     public static void main(String[] args) {
-        int[] testArray = {3,4,4 ,5,2,7,3,1,4,7,3,5};
-        System.out.println(smallest(testArray));
+
     }
 
     public static int smallest(int[] arr){
-        //int sm = arr[arr.length -1];
-        //System.out.println(sm);
-        int size = arr.length;
-        //System.out.println(size);
-        return smallest(arr, size, sm);
+        int lIndex = arr.length - 1;
+        return smallest(arr, lIndex);
     }
 
-    private static int smallest(int[] arr,  int size, int smallest){
-        if(size == 0){
-            return smallest;
-        }
-        else{
-            if (arr[size-1] < smallest){
-                smallest = arr[size-1];
-            }
-            return smallest(arr, size-1, smallest);
-
-        }
-    }
-
-    public static int cSmallest(int []arr){
-        return cSmallest(arr, arr.length-1);
-    }
-
-    private static int cSmallest(int []arr, int lastIndex){
-        int small = cSmallest(arr, lastIndex-1);
-        if (small < arr[lastIndex]) {
-            return small;
-        }
-        else {
+    private static int smallest(int[] arr,  int lastIndex){
+        if(lastIndex == 0){
             return arr[lastIndex];
         }
+        else {
+            int small = smallest(arr, lastIndex -1);
+            if(small < arr[lastIndex]){
+                return small;
+            }
+            else{
+                return arr[lastIndex];
+            }
+        }
     }
+
+    public static int binarySearch(int[] arr, int find){
+        int lIndex = arr.length - 1;
+        return binarySearch(arr, find, lIndex);
+    }
+
+    private static int binarySearch(int[] arr, int find, int lastIndex){
+        if(lastIndex == 0){
+            if(arr[lastIndex] == find){
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        }
+        else{
+            int index = binarySearch(arr, find, lastIndex -1);
+            if (index == -1){
+                if(arr[lastIndex] == find){
+                    return lastIndex;
+                }
+                else{
+                    return -1;
+                }
+            }
+            else{
+                return index;
+            }
+        }
+    }
+
 }
--- tests/Recursion/RecursiveTest.java
+++ tests/Recursion/RecursiveTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RecursiveTest {
 
     @Test
-    void smallestFirst() {
+    void smallest() {
         int[][] testingArrays = {
                 {1, 3, 5, 7},
                 {7, 5, 3, 2},
@@ -20,6 +20,25 @@ class RecursiveTest {
         for(int i=0; i<testingArrays.length; i++){
             int expected = exp[i];
             int res = Recursive.smallest(testingArrays[i]);
+            assertEquals(expected, res);
+        }
+    }
+
+    @Test
+    void BinarySearch() {
+        int[][] testingArrays = {
+                {1, 3, 5, 7},
+                {7, 5, 3, 2},
+                {5, 8, 3, 8, 7},
+                {2, 2, 2, 2},
+                {-4, 6, -10, 8}
+        };
+        int[] find = {1, 3, 7, 5, -10};
+        int[] exp = {0, 2, 4, -1, 2};
+
+        for(int i=0; i<testingArrays.length; i++){
+            int expected = exp[i];
+            int res = Recursive.binarySearch(testingArrays[i], find[i]);
             assertEquals(expected, res);
         }
     }
